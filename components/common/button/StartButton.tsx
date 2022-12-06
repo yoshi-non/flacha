d574d8a0025c2271898660f223af635b69f76883
yoshi-non <himekawa.yoshino44@gmail.com>
2022-12-06 23:54:30 +0900

    change: ファイルリファクタリング

--- components/common/button/StartButton.tsx
+++ components/common/button/StartButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from '../../../styles/Flash.module.css'
+import styles from '../../../styles/CommonBtn.module.css'
 
 const StartButton = () => {
     return (
