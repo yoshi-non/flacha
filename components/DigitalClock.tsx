97dd1b86decdb659e95539a5d482426ad8a3733d
yoshi-non <himekawa.yoshino44@gmail.com>
2022-12-07 09:42:57 +0900

    change: 日本時間を指定

--- components/DigitalClock.tsx
+++ components/DigitalClock.tsx
@@ -1,11 +1,16 @@
 import dayjs from 'dayjs'
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
 
 interface ClockNum {
     time: number
 }
 
+dayjs.extend(utc)
+dayjs.extend(timezone)
+
 const DigitalClock: React.VFC<ClockNum> = ({ time }) => {
-  return <div>{dayjs(time).format('HH:mm:ss')}</div>
+  return <div>{dayjs(time).tz("Asia/Tokyo").format('HH:mm:ss')}</div>
 }
 
 export default DigitalClock
