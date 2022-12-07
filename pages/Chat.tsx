b9e518e1595f3cb73330234849e2e260d5d9568f
yoshi-non <himekawa.yoshino44@gmail.com>
2022-12-07 10:11:47 +0900

    add: Chat設定画面

--- pages/Chat.tsx
+++ pages/Chat.tsx
@@ -1,16 +1,34 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
+import ReturnButton from '../components/common/button/ReturnButton'
+import StartButton from '../components/common/button/StartButton'
 import Layout from '../components/common/layout'
+import SelectDigitSetting from '../components/common/select/SelectDigitSetting'
+import SelectQuestionSetting from '../components/common/select/SelectQuestionSetting'
 import styles from '../styles/Chat.module.css'
 
 const Chat = () => {
+  const [selectQuestionValue,setSelectQuestionValue] = useState<number>(10)
+  const [selectDigitValue,setSelectDigitValue] = useState<number>(5)
+
   return (
     <Layout>
-      <Link href="/">ホームに戻る</Link>
+      <ReturnButton/>
       <div className={styles.container}>
-        <main className={styles.main}>
-          Chat
-        </main>
+
+        <div>
+          <div className={styles.selectContainer}>
+            <div className={styles.selectBoxHr}></div>
+            <div className={styles.selectBox}>
+              <SelectQuestionSetting setSelectQuestionValue={setSelectQuestionValue}/>
+              <SelectDigitSetting setSelectDigitValue={setSelectDigitValue}/>
+            </div>
+            <div className={styles.selectBoxHr}></div>
+          </div>
+
+          <StartButton/>
+        </div>
+
       </div>
     </Layout>
   )
