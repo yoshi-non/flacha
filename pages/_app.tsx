6f294cd3eb6a734fd0d0fe3fa8d3694b9387856a
yoshi-non <himekawa.yoshino44@gmail.com>
2022-12-09 21:15:41 +0900

    fix: fontawesome iconsバグ

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 import { AnimatePresence } from 'framer-motion'
 import type { AppProps } from 'next/app'
+import { config as FaSvgCoreConfig } from '@fortawesome/fontawesome-svg-core'
+
+FaSvgCoreConfig.autoAddCss = false
 
 const App = ({ Component, pageProps, router }: AppProps) => (
   <AnimatePresence mode="wait" onExitComplete={() => window.scrollTo(0, 0)}>
