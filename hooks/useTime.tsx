eea6d42623e0605db227a875ce306dd8ee958b5d
yoshi-non <himekawa.yoshino44@gmail.com>
2022-12-09 19:23:03 +0900

    fix: timeエラー

--- hooks/useTime.tsx
+++ hooks/useTime.tsx
@@ -1,10 +1,15 @@
 import { useEffect, useState } from 'react'
 
-export const useTime = (interval: number) => {
-  const [time, updateTime] = useState(Date.now())
+export const useTime = (interval: number): number | undefined => {
+  const [time, setTime] = useState<number>()
 
   useEffect(() => {
-    const timeoutId = setTimeout(() => updateTime(Date.now()), interval)
+    if(time == null) {
+      setTime(Date.now())
+      return
+    }
+
+    const timeoutId = setTimeout(() => setTime(Date.now()), interval)
     return () => {
       clearTimeout(timeoutId)
     }
